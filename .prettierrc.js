8cf3d8b0ff7843b2c73c3740077f4c54c302cfdf
Hugo Martinet <hugo.martinet@gmail.com>
2020-08-15 15:38:12 +0200

    setup repo

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
-  trailingComma: 'all',
-};
+  trailingComma: 'es6',
+  semi: false,
+  arrowParens: 'avoid',
+}
